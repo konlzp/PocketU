dd7462bc909cabe8c0e6188c4a36328cfe1494da
JIARUI DING <holmes93@163.com>
2015-12-06 10:33:06 -0500

    Initial Commit

---
+++ PocketU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PocketU
+//
+//  Created by 丁嘉瑞 on 15/12/6.
+//  Copyright © 2015年 Edward Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
